d002ee7d870d4720657b28bbbdc8eb0a40f29dee
Ammar Shakir <shakirammar12@gmail.com>
2020-08-19 22:07:13 +0000

    function for finding a value by key

---
+++ findKeyByValue.js
@@ -0,0 +1,15 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else  {
+    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = function (object, value) {
+  for (let key in object) {
+    if (object[key] === value) {
+      return key;
+    }
+  }
+};
