f7bdfb5dee0a5bca3fbedf9de1afe40755221076
Ammar Shakir <shakirammar12@gmail.com>
2020-08-19 19:06:16 +0000

    created a function that finds the middle value of the array

--- middle.js
+++ middle.js
@@ -1,13 +1,13 @@
 const eqArrays = function(array1, array2) {
-  if(array1.length !== array2.length) {
+  if (array1.length !== array2.length) {
     return false;
-  };
+  }
   for (let i = 0; i < array1.length; i++) {
     if (array1[i] !== array2[i]) {
       return false;
-    } 
+    }
   } return true;
-}
+};
 
 const assertArraysEqual = function(newArr1, newArr2) {
   if (eqArrays(newArr1, newArr2)) {
@@ -15,4 +15,17 @@ const assertArraysEqual = function(newArr1, newArr2) {
   } else {
     console.log("❌❌❌ Assertion Failed: " + newArr1 + " !== " + newArr2);
   }
-}
+};
+
+const middle = function(array) {
+  let middleElement = Math.ceil(array.length / 2);
+  let output = [];
+  if (array.length <= 2) {
+    return output;
+  } else if (array.length % 2 !== 0) {
+    output.push(middleElement);
+  } else if (array.length % 2 === 0) {
+    output.push(middleElement);
+    output.push(middleElement + 1);
+  } return output;
+};
