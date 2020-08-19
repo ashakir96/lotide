a2986e6c38ac10d3ba59b6b1514de752b3158438
Ammar Shakir <shakirammar12@gmail.com>
2020-08-19 03:24:53 +0000

    changed the nested for loop to work with the assertation function

--- without.js
+++ without.js
@@ -8,17 +8,17 @@ const eqArrays = function(array1, array2) {
 
 const assertArraysEqual = function(newArr1, newArr2) {
   if (eqArrays(newArr1, newArr2)) {
-    console.log("✅✅✅ Assertion Passed");
+    console.log("✅✅✅ Assertion Passed: " + newArr1 + " === " + newArr2);
   } else {
-    console.log("❌❌❌ Assertion Failed");
+    console.log("❌❌❌ Assertion Failed: " + newArr1 + " !== " + newArr2);
   }
 }
 
 const without = function (arr1, arr2) {
-  let newArray = arr1;
+  let newArray = Array.from(arr1);
   for (let i = 0; i < arr1.length; i++) {
     for (let j = 0; j < arr2.length; j++) {
-      if (arr1[i] === arr2[j]) {
+      if (newArray[i] === arr2[j]) {
         newArray.splice(i, 1);
       }
     }
