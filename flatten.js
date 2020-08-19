3284d8db592a9d1e35acfbf1bfbf8ceec3207a78
Ammar Shakir <shakirammar12@gmail.com>
2020-08-19 04:51:17 +0000

    created a function that removes nested arrays

---
+++ flatten.js
@@ -0,0 +1,33 @@
+const eqArrays = function(array1, array2) {
+  if(array1.length !== array2.length) {
+    return false;
+  };
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    } 
+  } return true;
+}
+
+const assertArraysEqual = function(newArr1, newArr2) {
+  if (eqArrays(newArr1, newArr2)) {
+    console.log("✅✅✅ Assertion Passed");
+  } else {
+    console.log("❌❌❌ Assertion Failed");
+  }
+}
+
+const flatten = function(array) {
+  let output = [];
+  for (let i = 0; i < array.length; i++){
+    if (Array.isArray(array[i])) {
+      for (let j = 0; j < array[i].length; j++) {
+        output.push(array[i][j]);
+      } 
+    } else {
+      output.push(array[i]);
+    }
+  } return output;
+}
+
+console.log(flatten([1, 2, [3, 4], 5, [6]]));
