870eb28712547b94ca92ef57bcfd0a387295007b
Ammar Shakir <shakirammar12@gmail.com>
2020-08-20 22:12:48 +0000

    finding keys in an object using callbacks

---
+++ findKey.js
@@ -0,0 +1,15 @@
+const findKey = function(object, callback) {
+  for (let item in object) {
+    if (callback(object[item])) {
+      return item;
+    }
+  }
+};
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else  {
+    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
