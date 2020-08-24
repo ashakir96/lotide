472e6c4d6f7bda52a60cc1b3dc37f7f894ce8075
Ammar Shakir <shakirammar12@gmail.com>
2020-08-24 20:44:28 +0000

    added tests to headTest.js using mocha chai

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,9 +1,6 @@
 const eqArrays = require("../eqArrays");
 const assertEqual = require("../assertEqual");
 
-console.log(eqArrays([1, 2, 3], [1, 2, 3]));
-console.log(eqArrays([1, 2, 3], [3, 2, 1]));
-
 eqArrays(["1", "2", "3"], ["1", "2", "3"]);
 eqArrays(["1", "2", "3"], ["1", "2", 3]);
 
