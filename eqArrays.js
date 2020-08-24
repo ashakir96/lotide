4576c26dec1f08a73c36522fc4b263bd1a6b55d0
Ammar Shakir <shakirammar12@gmail.com>
2020-08-24 20:00:18 +0000

    added a test file for eqArrays.js and removed test code from eqArrays.js

--- eqArrays.js
+++ eqArrays.js
@@ -1,10 +1,4 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else  {
-    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require("./assertEqual");
 
 const eqArrays = function(array1, array2) {
   if (array1.length !== array2.length) {
@@ -16,3 +10,5 @@ const eqArrays = function(array1, array2) {
     }
   } return true;
 };
+
+module.exports = eqArrays;
